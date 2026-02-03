499940ba3b4e4bbd5e4746ce021b0449f1b7f70e
ruets <dev@ruets.pro>
2026-02-03 17:29:45 +0100

    feat(EasySave.Models): add Job and JobType classes

--- EasySave.Models/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EasySave.Models;
-
-public class Class1
-{
-
-}
---
+++ EasySave.Models/Job.cs
@@ -0,0 +1,17 @@
+﻿namespace EasySave.Models;
+
+public class Job
+{
+  public string Name { get; set; }
+  public JobType Type { get; set; }
+  public string SourcePath { get; set; }
+  public string DestinationPath { get; set; }
+  
+  public Job(string name, JobType type, string sourcePath, string destinationPath)
+  {
+    Name = name;
+    Type = type;
+    SourcePath = sourcePath;
+    DestinationPath = destinationPath;
+  }
+}
