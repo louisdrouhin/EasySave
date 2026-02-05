f1bb2717e5592302ad3d06c2f89a296255662a60
betasown <louis.drouhin@gmail.com>
2026-02-05 10:33:04 +0100

    feat(configParser.cs): add configParser class

---
+++ EasySave.Core/JobManager.cs
@@ -0,0 +1,31 @@
+﻿namespace EasySave.Core;
+
+using EasySave.Models;
+using EasyLog.Lib;
+
+public class JobManager
+{
+    private readonly List<Job> _jobs;
+    private readonly EasyLog _logger;
+
+    public JobManager()
+    {
+        _jobs = new List<Job>();
+
+        var logFormatter = new JsonLogFormatter();
+        var logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", "easysave.log");
+        _logger = new EasyLog(logFormatter, logPath);
+    }
+
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    public void RemoveJob(Job job)
+    {
+        _jobs.Remove(job);
+    }
+
+
+}
